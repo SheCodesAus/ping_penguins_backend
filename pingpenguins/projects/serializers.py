1a76dc47fd2fbd857eb9014350439b7fbeb0f73e
Anaya <anaya.basak@gmail.com>
2025-01-20 15:47:42 +1000

    Created views, GET Board, POST Board.  Tests passed, screenshots attached to ticket.

--- pingpenguins/projects/serializers.py
+++ pingpenguins/projects/serializers.py
@@ -1,1 +1,21 @@
 from rest_framework import serializers
+from .models import Board, Category, Note
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+    notes = NoteSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class BoardSerializer(serializers.ModelSerializer):
+    categories = CategorySerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Board
+        fields = '__all__'
