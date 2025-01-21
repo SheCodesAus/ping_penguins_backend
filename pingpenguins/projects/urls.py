65e0e30c330751c58b37ee7f88c71369a5d33c93
Anaya <anaya.basak@gmail.com>
2025-01-21 15:24:26 +1000

    Added category serializers, urls and views.  Tested successful GET and POST in Insomnia

--- pingpenguins/projects/urls.py
+++ pingpenguins/projects/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('board/', views.BoardList.as_view()),
+    # path('board/<int:pk>/', views.BoardDetail.as_view()), This is to be updated once UUID functionality added
+    path('category/', views.CategoryList.as_view())
     ]
