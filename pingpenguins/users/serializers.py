9393a9cf190e6c007d43b03954fcc42c2b63491e
marina-terina <marinat@outlook.com.au>
2025-01-22 21:33:51 +1000

    added the functionality to create a new user + token auth

--- pingpenguins/users/serializers.py
+++ pingpenguins/users/serializers.py
@@ -2,10 +2,34 @@ from rest_framework import serializers
 from .models import CustomUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = CustomUser
-        fields = '__all__'
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = [
+            'id', 'username', 'email', 'password', 'confirm_password', 
+            'first_name', 'last_name', 'display_name', 'position', 
+            'gender', 'tenure', 'age', 'sticky_note_colour'
+        ]
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'email': {'required': True},
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'display_name': {'required': True},
+            'sticky_note_colour': {'required': True},
+        }
+
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError({"confirm_password": "Passwords do not match."})
+        return data
 
     def create(self, validated_data):
+        validated_data.pop('confirm_password')
         return CustomUser.objects.create_user(**validated_data)
+    
+class PublicUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'display_name']
