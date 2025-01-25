b07f5a48a52cdbec1138212bf0d31cea38ba0b5e
Anaya <anaya.basak@gmail.com>
2025-01-25 11:32:27 +1000

    Bring dev into the main history

--- pingpenguins/projects/serializers.py
+++ pingpenguins/projects/serializers.py
@@ -19,3 +19,17 @@ class BoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = apps.get_model('projects.Board')
         fields = '__all__'
+
+class BoardDetailSerializer(BoardSerializer):
+    notes = NoteSerializer(many=True, read_only=True)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.disclaimer = validated_data.get('disclaimer', instance.disclaimer)
+        instance.date_start = validated_data.get('date_start', instance.date_start)
+        instance.date_end = validated_data.get('date_end', instance.date_end)
+        instance.image = validated_data.get('image', instance.image)
+        instance.code = validated_data.get('code', instance.code)
+        instance.save()
+        return instance
